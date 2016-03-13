74223b515b6319e1804f61836748fb4d7d9fa084
matosmelo <felipe.matosmelo@gmail.com>
2016-03-13 13:01:10 -0300

    mais litio

---
+++ src/main/java/br/com/matosmelo/faceLitico/dao/NoticiaDAO.java
@@ -0,0 +1,59 @@
+package br.com.matosmelo.faceLitico.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.matosmelo.faceLitico.model.Litico;
+
+@Component
+public class NoticiaDAO {
+
+	private final Session session;
+
+	public NoticiaDAO(Session session) {
+		this.session = session;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Litico> listaLitico() {
+		return this.session.createQuery("select litico from Litico as litico").list();
+	}
+
+	public void salva(Litico litico) {
+		Transaction tx = this.session.beginTransaction();
+		session.save(litico);
+		tx.commit();
+	}
+
+	public void atualiza(Litico litico) {
+		Transaction tx = session.beginTransaction();
+		this.session.update(litico);
+		tx.commit();
+	}
+
+	public void remove(Litico litico) {
+		Transaction tx = session.beginTransaction();
+		this.session.delete(litico);
+		tx.commit();
+	}
+
+	// Carrega por ID
+	public Litico carrega(Long id) {
+		return (Litico) this.session.get(Litico.class, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Litico> carregaString(String string) {
+		return this.session.createQuery("select litico from Litico as litico where nome= '" + string + "' or partido= '"
+				+ string + "' or cargo= '" + string + "' or cidade= '" + string + "' or estado= '" + string
+				+ "' or localFisicoTabalho= '" + string + "' ").list();
+	}
+
+	public void recarrega(Litico litico) {
+		session.refresh(litico);
+	}
+
+}
